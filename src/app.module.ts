74335620b4b421171462304940e378a116acfe41
Arvin Kent Lazaga <arvinkent121816@gmail.com>
2023-03-03 23:54:29 +0800

    feat: added initial task files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { DatabaseModule } from './database/database.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksService } from './tasks/tasks.service';
+import { TasksController } from './tasks/tasks.controller';
+import { TaskModule } from './tasks/task.module';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AppConfigModule, DatabaseModule, TaskModule],
+  controllers: [AppController, TasksController],
+  providers: [AppService, TasksService],
 })
 export class AppModule {}
---
+++ src/tasks/task-status.enum.ts
@@ -0,0 +1,5 @@
+enum TaskStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
