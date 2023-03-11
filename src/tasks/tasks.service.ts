0d5fef34a06c79dc7a889f89669c5a49e54fb9da
Arvin Kent Lazaga <arvinkent121816@gmail.com>
2023-03-12 01:07:19 +0800

    feat: added update and delete for task service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,15 +1,17 @@
 import { DB_TASK_MODEL, ERR_UNPROCESSABLE_ENTITY } from './../constants';
 import {
   Injectable,
-  InternalServerErrorException,
+  NotFoundException,
   UnprocessableEntityException,
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskDocument } from './task.schema';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Task } from './task.interface';
 import { User } from '../users/user.interface';
+import { GetTaskDto } from './dto/get-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -23,41 +25,74 @@ export class TasksService {
    * @param {User} user - The user who will own the new task.
    * @param {CreateTaskDto} createTaskDto - The data required to create the new task.
    * @throws {UnprocessableEntityException} If a task with the same title already exists.
-   * @throws {InternalServerErrorException} If an unexpected error occurs while creating the new task.
    * @returns {Promise<Task>} The newly created Task object.
    */
   async createTask(user: User, createTaskDto: CreateTaskDto): Promise<Task> {
     const { title } = createTaskDto;
+    const isTaskExist = await this.findTask({ title });
 
-    if (await this.checkIfTaskExists(title)) {
+    if (isTaskExist) {
       throw new UnprocessableEntityException(ERR_UNPROCESSABLE_ENTITY);
     }
 
-    try {
-      const createdTask = this.taskModel.create({
-        ...createTaskDto,
-        userId: user.id,
-      });
-
-      return createdTask;
-    } catch (err) {
-      throw new InternalServerErrorException();
-    }
+    return this.taskModel.create({
+      ...createTaskDto,
+      userId: user.id,
+    });
   }
 
   /**
    * Checks if a task with the specified title already exists.
    *
-   * @param {string} title - The title of the task to check.
-   * @returns {Promise<Task>} A promise that resolves to the existing task, or `null` if it doesn't exist.
+   * @param {GetTaskDto} getTaskDto - The data required to find.
+   * @returns {Promise<Task | null>} A promise that resolves to the existing task, or `null` if it doesn't exist.
    */
-  async checkIfTaskExists(title: string): Promise<Task | null> {
-    const task = await this.taskModel.findOne({ title }).exec();
+  async findTask(getTaskDto: GetTaskDto): Promise<Task | null> {
+    const { title } = getTaskDto;
+    const task: Task | null = await this.taskModel.findOne({ title });
 
     if (!task) {
       return null;
     }
 
     return task;
+  }
+
+  /**
+   * Updates a task by ID
+   *
+   * @param {Types.ObjectId} id - The ID of the task to update
+   * @param {UpdateTaskDto} updateTaskDto - The updated task data
+   * @throws {NotFoundException} If no task is found with the given ID
+   * @returns {Promise<Task>} The updated task
+   */
+  async updateTask(
+    id: Types.ObjectId,
+    updateTaskDto: UpdateTaskDto,
+  ): Promise<Task> {
+    const task = await this.taskModel.findByIdAndUpdate(id, updateTaskDto, {
+      new: true,
+      useFindAndModify: false,
+    });
+    if (!task) {
+      throw new NotFoundException();
+    }
+
+    return task;
+  }
+
+  /**
+   * Deletes a task by ID
+   *
+   * @param {Types.ObjectId} id - The ID of the task to delete
+   * @throws {NotFoundException} If no task is found with the given ID
+   * @returns {Promise<void>}
+   */
+  async deleteTask(id: Types.ObjectId): Promise<void> {
+    const task = await this.taskModel.findByIdAndRemove(id);
+
+    if (!task) {
+      throw new NotFoundException();
+    }
   }
 }
