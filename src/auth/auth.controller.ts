d939947acc4d2efa5e7544108b43c8d81c3ea999
Arvin Kent Lazaga <arvinkent121816@gmail.com>
2023-03-12 17:04:44 +0800

    feat: added register route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,24 @@
-import { Body, Controller, Post, HttpStatus, HttpCode } from '@nestjs/common';
-import { ApiTags, ApiBody, ApiResponse } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpStatus,
+  HttpCode,
+  UnprocessableEntityException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import {
+  ApiTags,
+  ApiBody,
+  ApiResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { Token } from './token.interface';
 import { Public } from '../common/decorators/public.decorator';
+import { RegisterDto } from './dto/register.dto';
+import { User } from '../users/user.interface';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -14,10 +29,11 @@ export class AuthController {
    * Logs in a user and returns a JWT token.
    *
    * @param {LoginDto} loginDto - The DTO containing the user's login credentials.
+   * @throws {UnauthorizedException} If invalid user credentials
    * @returns {Promise<Token>} A JWT token.
    */
   @ApiResponse({ status: HttpStatus.OK })
-  @ApiResponse({ status: HttpStatus.UNAUTHORIZED })
+  @ApiUnauthorizedResponse({ status: HttpStatus.UNAUTHORIZED })
   @ApiBody({
     type: LoginDto,
   })
@@ -26,5 +42,33 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: LoginDto): Promise<Token> {
     return this.authService.login(loginDto);
+  }
+
+  /**
+   * Register an account for a user.
+   *
+   * @param {RegisterDto} registerDto - The data required for registering the user.
+   * @throws {UnprocessableEntityException} If the user already exists.
+   * @throws {InternalServerErrorException} If there is an internal server error.
+   * @returns {Promise<User>} The newly registered user.
+   */
+  @ApiBody({
+    type: RegisterDto,
+  })
+  @HttpCode(HttpStatus.CREATED)
+  @Public()
+  @Post('register')
+  async register(@Body() registerDto: RegisterDto): Promise<User> {
+    try {
+      const registerUser = this.authService.register(registerDto);
+
+      return registerUser;
+    } catch (error) {
+      if (error instanceof UnprocessableEntityException) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException();
+    }
   }
 }
